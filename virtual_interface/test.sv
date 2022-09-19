5a06fd9faf5fe4adbf54997cc229e245f0967cf2
DilipKrishnappa <dilipkrishnappa@HweServer.mirafra.com>
2022-09-19 21:42:49 +0530

    virtual_interface

--- virtual_interface/test.sv
+++ virtual_interface/test.sv
@@ -1,9 +1,17 @@
+//include the driver file
 `include "driver.sv"
+//module:test
 module test(adder intf);
+
+//declaring the driver instances
 driver drv;
+
 initial
 begin
+  //creating the driver instance
   drv = new(intf);
+
+  // calling the task run
   drv.run();
 end
-endmodule
+endmodule:test
